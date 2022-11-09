1725553293095120777c48275814464127357870
reyeon1209 <reyeon5368@naver.com>
2022-11-09 10:31:41 +0900

    Add scss, Change structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,25 @@
 <script>
-	import { writable } from 'svelte/store';
-	import Todo from './Todo.svelte';
-	
-	let title = '';
-	let todos = writable([]);
-	let id = 0;	// crypto-random-string, uuid 등 외부라이브러리 사용 가능
-	
-	function createTodo() {
-		if (!title.trim()) { title = ''; alert('Empty!'); return }
-		$todos.push({ id, title });	// id: id, title: title
-		$todos = $todos;	// 반응성
-		title = '';
-		id += 1;
-	}
+  import { todos } from '~/store'
+  import CreateTodo from '~/components/CreateTodo'
+  import Todo from '~/components/Todo'
+
+  const storageTodos = localStorage.getItem('todos')
+  if (storageTodos) {
+    $todos = JSON.parse(storageTodos)
+  }
 </script>
 
-<input bind:value={title} type="text"
-	on:keydown={(e) => { e.key == 'Enter' && createTodo() }}/>
-<!--
-	if (e.key === 'Enter') { createTodo() }
-	e.key === 'Enter' ? createTodo() : undefined
-	e.key === 'Enter' && createTodo()
--->
-<button on:click={createTodo}>Create Todo</button>
+<div class="container">
+  <CreateTodo />
+  <div class="todos">
+    {#each $todos as todo (todo.id)}
+      <Todo {todo} />
+    {/each}
+  </div>
+</div>
 
-{#each $todos as todo}
-	<Todo {todos} {todo} />
-{/each}
+<style lang="scss">
+  .todos {
+    padding: 20px 0;
+  }
+</style>
